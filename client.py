94d9cc4bafa84e5a1c6d374b81aadc57a5f00701
alberto911 <alberto911_@hotmail.com>
2017-05-15 10:57:56 +0000

    Aiocoap client example

---
+++ client.py
@@ -0,0 +1,23 @@
+import sys
+import logging
+import asyncio
+
+from aiocoap import *
+
+logging.basicConfig(level=logging.INFO)
+
+async def client(service_path):
+    protocol = await Context.create_client_context()
+
+    request = Message(code=GET, uri=service_path)
+
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+    else:
+        print('Result: %s\n%r'%(response.code, response.payload))
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(client(sys.argv[1]))
