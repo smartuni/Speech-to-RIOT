76045b572d56749d89f3ce71f0213e6d8a38b5c9
ThieAr <arne.thiele@haw-hamburg.de>
2017-06-12 08:04:21 +0000

    Discovery 98% fertig

--- Scanner.py
+++ Scanner.py
@@ -6,12 +6,11 @@ from aiocoap import *
 
 def scanIps():
     ips = set()
-    result = subprocess.run(['ping6', '-c', '1', 'ff02::1%lowpan0'], stdout=subprocess.PIPE)
+    result = subprocess.run(['ping6', '-c', '5', 'ff02::1%lowpan0'], stdout=subprocess.PIPE)
 
     regex = r"\d* bytes from ([0-9,a-f]*::[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*:[0-9,a-f]*): icmp_seq=\d* ttl=\d* time=\d*.\d*"
 
     matches = re.finditer(regex, str(result.stdout))
-    print(str(result.stdout))
 
     for matchNum, match in enumerate(matches):
         matchNum = matchNum + 1
@@ -19,22 +18,14 @@ def scanIps():
         for groupNum in range(0, len(match.groups())):
             groupNum = groupNum + 1
             ip = match.group(groupNum)
-            print("{group}".format(group=match.group(groupNum)))
             ips.add(ip)
 
     return ips
 
-<<<<<<< HEAD
-print(scanIps())
-=======
-
-print(scanIps())
-
-
 async def main(ip):
     protocol = await Context.create_client_context()
 
-    request = Message(code=GET, uri='coap://[' + ip + ']/.well-known/core', mtype=NON)
+    request = Message(code=GET, uri='coap://[' + ip + '%lowpan0]/.well-known/core', mtype=NON)
 
     try:
         response = await protocol.request(request).response
@@ -42,15 +33,10 @@ async def main(ip):
         print('Failed to fetch resource:')
         print(e)
     else:
-        print(response.payload)
         return response.payload
 
-
-if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
-
 ipset = scanIps()
+print(ipset)
 for ip in ipset:
-    result = main(ip)
-    print(result + ' \n')
->>>>>>> 275be38d7f949b83d5eb2c4ca32dc80289abb55e
+    result = asyncio.get_event_loop().run_until_complete(main(ip))
+    print(str(result) + ' \n')
